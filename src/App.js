d66128e167a14096818f0261b60ad61259eb58e1
thisisannak23 <anna.kazanecki@gmail.com>
2022-07-28 14:23:34 -0500

    New components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import logo from "./orangeIcon.png";
+import Dictionary from "./dictionary.js";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header d-block">
-        <img
-          src={logo}
-          className="App-logo "
-          width="100px"
-          alt="Orange Dot Logo"
-        />
-      </header>
+    <div className="App text-center">
+      <div className="container">
+        <header className="App-header d-block">
+          <img
+            src={logo}
+            className="App-logo "
+            width="100px"
+            alt="Orange Dot Logo"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <small className="credit bg-info">Coded by Anna</small>
+      </div>
     </div>
   );
 }
---
+++ src/dictionary.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          className="searchBar"
+          onChange={handleKeywordChange}
+          placeholder="type word here"
+          autoFocus={true}
+        />
+      </form>
+    </div>
+  );
+}
