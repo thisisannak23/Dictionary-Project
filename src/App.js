40817a47eb87377faaf983010d363b01cc0337cb
thisisannak23 <anna.kazanecki@gmail.com>
2022-07-28 13:22:27 -0500

    Set up and organized

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./orangeIcon.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header d-block">
+        <img
+          src={logo}
+          className="App-logo "
+          width="100px"
+          alt="Orange Dot Logo"
+        />
+      </header>
+    </div>
+  );
+}
